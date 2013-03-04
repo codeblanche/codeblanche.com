cc11c98469c39938e8335a9b660167dd7db3190c
Merten van Gerven <merten.vg@gmail.com>
2013-03-04 12:26:31 +0100

    Slim framework implementation

--- public/index.php
+++ public/index.php
@@ -1,7 +1,15 @@
 <?php
 
-/*
-require 'construction.html';
-/*/
-require 'index.html';
-//*/
+require '../vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+$app->get('/', function () {
+    require 'index.html';
+});
+
+$app->get('/:page', function ($page) {
+    require 'index.html';
+});
+
+$app->run();
